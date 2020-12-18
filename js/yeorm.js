1fcc808b13c56539d3a60b6affa7e841f395e96b
Luminon <atluminon@itsuka.space>
2020-12-18 17:46:12 +0900

    [FIXED] Added Outoscroll

--- js/yeorm.js
+++ js/yeorm.js
@@ -6,19 +6,27 @@ const areaExpandA = document.getElementById("area-a");
 const areaExpandB = document.getElementById("area-b");
 const areaExpandC = document.getElementById("area-c");
 const areaExpandD = document.getElementById("area-d");
+const locationA = document.getElementById("article-a").offsetTop;
+const locationB = document.getElementById("article-b").offsetTop;
+const locationC = document.getElementById("article-c").offsetTop;
+const locationD = document.getElementById("article-d").offsetTop;
 
 btnExpandA.addEventListener('click', () => {
     areaExpandA.classList.toggle('expand');
+    window.scrollTo({top:locationA, behavior:'smooth'});
 });
 
 btnExpandB.addEventListener('click', () => {
     areaExpandB.classList.toggle('expand');
+    window.scrollTo({top:locationB, behavior:'smooth'});
 });
 
 btnExpandC.addEventListener('click', () => {
     areaExpandC.classList.toggle('expand');
+    window.scrollTo({top:locationC, behavior:'smooth'});
 });
 
 btnExpandD.addEventListener('click', () => {
     areaExpandD.classList.toggle('expand');
+    window.scrollTo({top:locationD, behavior:'smooth'});
 });
